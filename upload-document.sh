c9fbe4fe4bba529bcc9a77281b71d9655e1eaec0
Adrian Hornsby <adhorn@amazon.com>
2019-09-18 14:29:35 +0300

    adding tags

--- upload-document.sh
+++ upload-document.sh
@@ -7,5 +7,5 @@ done
 for file in *.yml
     do
         file_noext=${file%.*}
-        aws ssm create-document --content file://$file --name "$file_noext" --document-type "Command" --document-format YAML --region $REGION
+        aws ssm create-document --content file://$file --name "$file_noext" --document-type "Command" --document-format YAML --tags "Key=SSMtag,Value=chaos" --region $REGION
 done
